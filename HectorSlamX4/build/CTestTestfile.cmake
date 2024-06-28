fa553087d635c351a84e4d315a0ae5bd41e4ae30
Tirthankar <tirthankarhalder70@gmail.com>
2024-06-28 12:31:46 +0530

    Hectorslam x4 added

---
+++ HectorSlamX4/build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/prachi/ydlidar_ws/src
+# Build directory: /home/prachi/ydlidar_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("ydlidar")
+subdirs("ydlidar_ros_driver")
