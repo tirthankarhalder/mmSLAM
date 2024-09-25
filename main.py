e752771bd1a8304732ac7c493a3ffe3ad2f0defd
Tirthankar-Halder <tirthankarhalder70@gmail.com>
2024-09-25 22:25:34 +0530

    main edit

--- main.py
+++ main.py
@@ -19,122 +19,123 @@ def pointcloud_openradar(file_name):
     pcd_data, time = generate_pcd_time(bin_filename, info_dict)
     # print(pcd_data.shape)
     return pcd_data, time
-        
-datasetsFolderPath = './datasets/'
-radarFilePath = os.path.join(datasetsFolderPath,"radar_data/")
-depthFilePath = os.path.join(datasetsFolderPath,"depth_data/")
-filteredBinFile = [f for f in os.listdir(radarFilePath) if os.path.isfile(os.path.join(radarFilePath, f)) and f.endswith('.bin') and not f.startswith('only_sensor')]
-filteredCsvFile = [f for f in os.listdir(depthFilePath) if os.path.isfile(os.path.join(depthFilePath, f)) and f.endswith('.csv') and not f.startswith('only_sensor')]
-
-total_framePCD = []
-total_framePCDDf = pd.DataFrame(columns=["datetime","pcd"])
-for file in filteredBinFile:#interate over all bin and stack
-    binFilePath = radarFilePath+file
-    gen,timestamps=point_cloud_frames(file_name =binFilePath)
-    for pointcloud in gen:
-        total_framePCD.append(pointcloud[:,:3])#sliced 1st 3 as x y z
-    df = pd.DataFrame()
-    df["datetime"] = timestamps
-    df["pcd"] = gen
-    df['datetime'] = pd.to_datetime(df['datetime'], format='%Y-%m-%d %H_%M_%S.%f')
-    saveCsv = radarFilePath + "csv_file/" + file.split["."][0] + ".csv"
-    if not os.path.exists(saveCsv):
-        os.makedirs(saveCsv)
-    total_framePCDDf.append(df)
-    df.to_csv(saveCsv)
-
-total_frameStacked = np.stack(total_framePCD)
-print("total_frameStacked.shape: ",total_frameStacked.shape)
-
-
-# total_frameDepth = pd.DataFrame()
-# for file in filteredCsvFile:#iterate over all csv and stack as df
-#     csvFilePath = depthFilePath+file
-#     depthDf = pd.read_csv(csvFilePath)
-
-csvFilePath = depthFilePath + "*.csv"
-dfs = [pd.read_csv(file) for file in csvFilePath]
-total_frameDepth = pd.concat(dfs, ignore_index=True)#merger all csv
-
-total_frameDepth['datetime'] = pd.to_datetime(total_frameDepth['datetime'], format='%Y-%m-%d %H_%M_%S.%f')
-total_frameDepth.dropna()
-total_frameDepth.to_csv("total_frameDepth.csv")
-
-#check new samples radar data plot with pcd column
-sns.set(style="whitegrid")
-fig = plt.figure(figsize=(12,7))
-ax = fig.add_subplot(111,projection='3d')
-for index,row in total_frameDepth.iterrows():
-    img = ax.scatter(total_framePCDDf["pcd"][0][:,0], total_framePCDDf["pcd"][0][:,1],total_framePCDDf["pcd"][0][:,2], cmap="viridis",marker='o')
-    fig.colorbar(img)
-    ax.set_xlabel('X')
-    ax.set_ylabel('Y')
-    ax.set_zlabel('Z')
-
-    plt.savefig("./visualization/radar/radar_"+ str(index)+".png")
-    # plt.show()
-    plt.close()
-    if index == 0:
-        break
-
-# check new samples depth data plot with pcdDepth column
-sns.set(style="whitegrid")
-fig = plt.figure(figsize=(12,7))
-ax = fig.add_subplot(111,projection='3d')
-for index,row in total_frameDepth.iterrows():
-    
-    lisx = np.array(ast.literal_eval(total_frameDepth["depthPCD"][0]))[:,0]
-    lisy = np.array(ast.literal_eval(total_frameDepth["depthPCD"][0]))[:,1]
-    lisz = np.array(ast.literal_eval(total_frameDepth["depthPCD"][0]))[:,2]
-    # print(li[0])
-    img = ax.scatter(lisx, lisy,lisz, cmap="viridis",marker='o')
-    fig.colorbar(img)
-    ax.set_xlabel('X')
-    ax.set_ylabel('Y')
-    ax.set_zlabel('Z')
-
-    plt.savefig("./visualization/depth/depthFrame_"+ str(index)+".png")
-    # plt.show()
-    plt.close()
-    if index == 0:
-        break
-
-
-# check new samples depth data plot with individual x y z column
-sns.set(style="whitegrid")
-fig = plt.figure(figsize=(12,7))
-ax = fig.add_subplot(111,projection='3d')
-for index,row in total_frameDepth.iterrows():
-    
-    lisx = ast.literal_eval(row["x"])
-    lisy = ast.literal_eval(row["y"])
-    lisz = ast.literal_eval(row["z"])
-    # print(li[0])
-    img = ax.scatter(lisx, lisy,lisz, cmap="viridis",marker='o')
-    fig.colorbar(img)
-    ax.set_xlabel('X')
-    ax.set_ylabel('Y')
-    ax.set_zlabel('Z')
-
-    plt.savefig("./visualization/depth/depthXYZ_"+ str(index)+".png")
-    # plt.show()
-    plt.close()
-    if index == 0:
-        break
-
-
-#totalframePCDDf and total_frameDepth is final merged file 
-
-mergerdPcdDepth = pd.merge_asof(total_framePCDDf, total_frameDepth, on='datetime',tolerance=pd.Timedelta('2us'), direction='nearest')
-
-print("mergerdPcdDepth.shape: ",mergerdPcdDepth.shape)
-
-
-
-
 
 
 if __name__ == "__main__":
+                
+    datasetsFolderPath = './datasets/'
+    radarFilePath = os.path.join(datasetsFolderPath,"radar_data/")
+    depthFilePath = os.path.join(datasetsFolderPath,"depth_data/")
+    filteredBinFile = [f for f in os.listdir(radarFilePath) if os.path.isfile(os.path.join(radarFilePath, f)) and f.endswith('.bin') and not f.startswith('only_sensor')]
+    filteredCsvFile = [f for f in os.listdir(depthFilePath) if os.path.isfile(os.path.join(depthFilePath, f)) and f.endswith('.csv') and not f.startswith('only_sensor')]
+
+    total_framePCD = []
+    total_framePCDDf = pd.DataFrame(columns=["datetime","pcd"])
+    for file in filteredBinFile:#interate over all bin and stack
+        binFilePath = radarFilePath+file
+        gen,timestamps=generate_pcd_time(file_name =binFilePath)
+        for pointcloud in gen:
+            total_framePCD.append(pointcloud[:,:3])#sliced 1st 3 as x y z
+        df = pd.DataFrame()
+        df["datetime"] = timestamps
+        df["pcd"] = gen
+        df['datetime'] = pd.to_datetime(df['datetime'], format='%Y-%m-%d %H_%M_%S.%f')
+        saveCsv = radarFilePath + "csv_file/" + file.split["."][0] + ".csv"
+        if not os.path.exists(saveCsv):
+            os.makedirs(saveCsv)
+        total_framePCDDf.append(df)
+        df.to_csv(saveCsv)
+
+    total_frameStacked = np.stack(total_framePCD)
+    print("total_frameStacked.shape: ",total_frameStacked.shape)
+
+
+    # total_frameDepth = pd.DataFrame()
+    # for file in filteredCsvFile:#iterate over all csv and stack as df
+    #     csvFilePath = depthFilePath+file
+    #     depthDf = pd.read_csv(csvFilePath)
+
+    csvFilePath = depthFilePath + "*.csv"
+    dfs = [pd.read_csv(file) for file in csvFilePath]
+    total_frameDepth = pd.concat(dfs, ignore_index=True)#merger all csv
+
+    total_frameDepth['datetime'] = pd.to_datetime(total_frameDepth['datetime'], format='%Y-%m-%d %H_%M_%S.%f')
+    total_frameDepth.dropna()
+    total_frameDepth.to_csv("total_frameDepth.csv")
+
+    #check new samples radar data plot with pcd column
+    sns.set(style="whitegrid")
+    fig = plt.figure(figsize=(12,7))
+    ax = fig.add_subplot(111,projection='3d')
+    for index,row in total_frameDepth.iterrows():
+        img = ax.scatter(total_framePCDDf["pcd"][0][:,0], total_framePCDDf["pcd"][0][:,1],total_framePCDDf["pcd"][0][:,2], cmap="viridis",marker='o')
+        fig.colorbar(img)
+        ax.set_xlabel('X')
+        ax.set_ylabel('Y')
+        ax.set_zlabel('Z')
+
+        plt.savefig("./visualization/radar/radar_"+ str(index)+".png")
+        # plt.show()
+        plt.close()
+        if index == 0:
+            break
+
+    # check new samples depth data plot with pcdDepth column
+    sns.set(style="whitegrid")
+    fig = plt.figure(figsize=(12,7))
+    ax = fig.add_subplot(111,projection='3d')
+    for index,row in total_frameDepth.iterrows():
+        
+        lisx = np.array(ast.literal_eval(total_frameDepth["depthPCD"][0]))[:,0]
+        lisy = np.array(ast.literal_eval(total_frameDepth["depthPCD"][0]))[:,1]
+        lisz = np.array(ast.literal_eval(total_frameDepth["depthPCD"][0]))[:,2]
+        # print(li[0])
+        img = ax.scatter(lisx, lisy,lisz, cmap="viridis",marker='o')
+        fig.colorbar(img)
+        ax.set_xlabel('X')
+        ax.set_ylabel('Y')
+        ax.set_zlabel('Z')
+
+        plt.savefig("./visualization/depth/depthFrame_"+ str(index)+".png")
+        # plt.show()
+        plt.close()
+        if index == 0:
+            break
+
+
+    # check new samples depth data plot with individual x y z column
+    sns.set(style="whitegrid")
+    fig = plt.figure(figsize=(12,7))
+    ax = fig.add_subplot(111,projection='3d')
+    for index,row in total_frameDepth.iterrows():
+        
+        lisx = ast.literal_eval(row["x"])
+        lisy = ast.literal_eval(row["y"])
+        lisz = ast.literal_eval(row["z"])
+        # print(li[0])
+        img = ax.scatter(lisx, lisy,lisz, cmap="viridis",marker='o')
+        fig.colorbar(img)
+        ax.set_xlabel('X')
+        ax.set_ylabel('Y')
+        ax.set_zlabel('Z')
+
+        plt.savefig("./visualization/depth/depthXYZ_"+ str(index)+".png")
+        # plt.show()
+        plt.close()
+        if index == 0:
+            break
+
+
+    #totalframePCDDf and total_frameDepth is final merged file 
+
+    mergerdPcdDepth = pd.merge_asof(total_framePCDDf, total_frameDepth, on='datetime',tolerance=pd.Timedelta('2us'), direction='nearest')
+
+    print("mergerdPcdDepth.shape: ",mergerdPcdDepth.shape)
+
+
+
+
+
 
     batch_size = 32
 
